d3d974b121b4bba6517cf987be6d5ce757de6e1f
Aman-Jaiswal <antooaman@gmail.com>
2018-02-18 09:28:46 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
 console.log('Loaded!');
+
+//Change the text of the main-text div
+var element=document.getElementById("main-text");
+
+element.innerHTML("New Value");
+
+var img= document.getElementById("madi");
+img.onclick=function(){
+    img.style.marginLeft="100 px";
+};
