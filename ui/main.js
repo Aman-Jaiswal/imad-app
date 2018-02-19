2b90b2b7a68cca9f06bdabf272132af8f8250dee
Aman-Jaiswal <antooaman@gmail.com>
2018-02-19 12:12:23 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,18 @@
+//submit btn
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit');
+submit.onclick = function(){
+    var names = ['name1', 'name2', 'name3', 'name4'];
+    var list = '';
+    
+    for(var i=0; i<names.length; i++){
+        list += '<li>'+ names[i] + '<li>';
+    }
+    var ul=document.getElementById('list');
+    ul.innnerHTML= list;
+};
 
 
 //Counter Code
