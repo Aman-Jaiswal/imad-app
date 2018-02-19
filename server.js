f3efaf70bf84a789803ee199d436934994401ac0
Aman-Jaiswal <antooaman@gmail.com>
2018-02-19 12:45:18 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,6 +98,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var names =[];
+app.get('/submit-name',function(req,res){
+    var name = req.query.name;
+    names.push(name);
+    res.send(JSON.stringfy(names));
+});
+
 var counter=0;
 app.get('/counter',function(req,res){
     counter = counter + 1;
