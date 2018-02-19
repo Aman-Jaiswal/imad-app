9db229d38458f979bf3738aadcb3a265fef2c0da
Aman-Jaiswal <antooaman@gmail.com>
2018-02-19 06:39:09 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,8 +19,10 @@ img.onclick = function(){
 //Counter Code
 var button = document.getElementById('counter');
 var counter = 0;
+
 button.onclick = function(){
-    counter = counter+1;
+    // Render the variable in correct span
+    counter = counter + 1;
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
 };
