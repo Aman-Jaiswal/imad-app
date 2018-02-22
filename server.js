51d4d0bbff69b96e0ee0d98b3bb779db8ca0cab4
Aman-Jaiswal <antooaman@gmail.com>
2018-02-22 06:43:03 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -132,9 +132,22 @@ app.get('/counter',function(req,res){
     res.send(counter.toString());
 });
 
-app.get('/:articleName',function(req,res){
-  var articleName=req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
+app.get('/articles/:articleName',function(req,res){
+  pool.query("SELECT * FROM articles WHERE title = '"+ req.params.articleName+"'", function(err,result){
+      if(err){
+          res.status(500).send(err.toString());
+      }
+      else{
+          if(result.rows.length=== 0){
+              res.status(404).send('Article Not Found');
+          }
+          else{
+              var articleData = result.rows[0];
+              res.send(createTemplate(articles[articleName]));
+          }
+      }
+  });
+  
 });
 
 app.get('/ui/style.css', function (req, res) {
